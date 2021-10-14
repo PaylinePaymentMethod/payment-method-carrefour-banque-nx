21c39fd3663551111a0b5667f26d1593d8e67a7e
Paul-Emile SUBLET <paul-emile.sublet@external.monext.net>
2021-10-14 14:23:14 +0200

    PAYLSQUAD3-1291 : Ajout du miscData pour le paiement

--- src/test/java/com/payline/payment/carrefour/banque/nx/mapper/FinancingRequestMapperTest.java
+++ src/test/java/com/payline/payment/carrefour/banque/nx/mapper/FinancingRequestMapperTest.java
@@ -1,16 +1,14 @@
 package com.payline.payment.carrefour.banque.nx.mapper;
 
 import com.payline.payment.carrefour.banque.nx.MockUtils;
-import com.payline.payment.carrefour.banque.nx.bean.request.CaptureTime;
-import com.payline.payment.carrefour.banque.nx.bean.request.Channel;
-import com.payline.payment.carrefour.banque.nx.bean.request.DeliveryMode;
-import com.payline.payment.carrefour.banque.nx.bean.request.FinancingRequest;
+import com.payline.payment.carrefour.banque.nx.bean.request.*;
 import com.payline.payment.carrefour.banque.nx.utils.Constants;
 import com.payline.pmapi.bean.payment.request.PaymentRequest;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -25,101 +23,110 @@ class FinancingRequestMapperTest {
 
         final FinancingRequest request = FinancingRequestMapper.INSTANCE.map(paymentRequest);
 
-        assertHeader(request);
-        assertRequest(request);
-        assertCustomer(request);
+        assertHeader(request.getHeader());
+        assertRequest(request.getRequest());
+        assertCustomer(request.getCustomer());
     }
 
-    private void assertHeader(final FinancingRequest request) throws ParseException {
-        assertEquals("MERCHANT_ID", request.getHeader().getMerchantId());
-        assertEquals("PAYLINE20200901181833", request.getHeader().getMessageId());
-        assertNotNull(sdf.parse(request.getHeader().getCreationDateTime()));
-        assertEquals("http://redirectionURL.com", request.getHeader().getRedirectPaymentSuccessUrl());
-        assertEquals("http://redirectionURL.com", request.getHeader().getRedirectPaymentFailureUrl());
-        assertEquals("http://redirectionCancelURL.com", request.getHeader().getRedirectPaymentCancelUrl());
-        assertEquals("http://notificationURL.com", request.getHeader().getCallbackUrl());
+    private void assertHeader(final Header header) throws ParseException {
+        assertEquals("MERCHANT_ID", header.getMerchantId());
+        assertEquals("PAYLINE20200901181833", header.getMessageId());
+        assertNotNull(sdf.parse(header.getCreationDateTime()));
+        assertEquals("http://redirectionURL.com", header.getRedirectPaymentSuccessUrl());
+        assertEquals("http://redirectionURL.com", header.getRedirectPaymentFailureUrl());
+        assertEquals("http://redirectionCancelURL.com", header.getRedirectPaymentCancelUrl());
+        assertEquals("http://notificationURL.com", header.getCallbackUrl());
     }
 
-    private void assertRequest(final FinancingRequest request) throws ParseException {
-        assertEquals("tbc1", request.getRequest().getOfferId());
-        assertEquals(4, request.getRequest().getDuration());
-        assertEquals(CaptureTime.PAYMENT, request.getRequest().getCaptureTime());
-        assertEquals("REF20200901181833", request.getRequest().getOrder().getOrderId());
-        assertEquals(Channel.DESKTOP, request.getRequest().getOrder().getChannel());
-        assertNotNull(sdf.parse(request.getRequest().getOrder().getOrderDateTime()));
+    private void assertRequest(final Request request) throws ParseException {
+        assertEquals("tbc1", request.getOfferId());
+        assertEquals(4, request.getDuration());
+        assertEquals(CaptureTime.PAYMENT, request.getCaptureTime());
+        assertEquals("REF20200901181833", request.getOrder().getOrderId());
+        assertEquals(Channel.DESKTOP, request.getOrder().getChannel());
+        assertNotNull(sdf.parse(request.getOrder().getOrderDateTime()));
 
         // cart
-        assertEquals("10.00", request.getRequest().getOrder().getCart().getTotalFinancedAmount());
-        assertEquals("EUR", request.getRequest().getOrder().getCart().getCurrency());
-        assertEquals(DeliveryMode.PICKUP_AT_MERCHANT, request.getRequest().getOrder().getCart().getDeliveryMode());
-        assertEquals("192.168.0.1", request.getRequest().getOrder().getCart().getIpV4());
-        assertNull(request.getRequest().getOrder().getCart().getIpV6());
+        assertEquals("10.00", request.getOrder().getCart().getTotalFinancedAmount());
+        assertEquals("EUR", request.getOrder().getCart().getCurrency());
+        assertEquals(DeliveryMode.PICKUP_AT_MERCHANT, request.getOrder().getCart().getDeliveryMode());
+        assertEquals("192.168.0.1", request.getOrder().getCart().getIpV4());
+        assertNull(request.getOrder().getCart().getIpV6());
 
         // cart items
-        assertEquals(2, request.getRequest().getOrder().getCart().getCartItems().size());
+        assertEquals(2, request.getOrder().getCart().getCartItems().size());
 
         // first cart item
-        assertEquals("20.00", request.getRequest().getOrder().getCart().getCartItems().get(0).getTotalPrice());
-        assertEquals("10.00", request.getRequest().getOrder().getCart().getCartItems().get(0).getUnitPrice());
-        assertEquals("EUR", request.getRequest().getOrder().getCart().getCartItems().get(0).getCurrency());
-        assertEquals("2", request.getRequest().getOrder().getCart().getCartItems().get(0).getQuantity());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(0).getUniverse());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(0).getCategory());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(0).getBrand());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(0).getModel());
-        assertEquals("ref", request.getRequest().getOrder().getCart().getCartItems().get(0).getReference());
-        assertEquals("comment?", request.getRequest().getOrder().getCart().getCartItems().get(0).getDescription());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(0).getPictureUri());
+        assertEquals("20.00", request.getOrder().getCart().getCartItems().get(0).getTotalPrice());
+        assertEquals("10.00", request.getOrder().getCart().getCartItems().get(0).getUnitPrice());
+        assertEquals("EUR", request.getOrder().getCart().getCartItems().get(0).getCurrency());
+        assertEquals("2", request.getOrder().getCart().getCartItems().get(0).getQuantity());
+        assertNull(request.getOrder().getCart().getCartItems().get(0).getUniverse());
+        assertNull(request.getOrder().getCart().getCartItems().get(0).getCategory());
+        assertNull(request.getOrder().getCart().getCartItems().get(0).getBrand());
+        assertNull(request.getOrder().getCart().getCartItems().get(0).getModel());
+        assertEquals("ref", request.getOrder().getCart().getCartItems().get(0).getReference());
+        assertEquals("comment?", request.getOrder().getCart().getCartItems().get(0).getDescription());
+        assertNull(request.getOrder().getCart().getCartItems().get(0).getPictureUri());
 
         // second cart item
-        assertEquals("30.00", request.getRequest().getOrder().getCart().getCartItems().get(1).getTotalPrice());
-        assertEquals("10.00", request.getRequest().getOrder().getCart().getCartItems().get(1).getUnitPrice());
-        assertEquals("EUR", request.getRequest().getOrder().getCart().getCartItems().get(1).getCurrency());
-        assertEquals("3", request.getRequest().getOrder().getCart().getCartItems().get(1).getQuantity());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(1).getUniverse());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(1).getCategory());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(1).getBrand());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(1).getModel());
-        assertEquals("ref2", request.getRequest().getOrder().getCart().getCartItems().get(1).getReference());
-        assertEquals("comment2", request.getRequest().getOrder().getCart().getCartItems().get(1).getDescription());
-        assertNull(request.getRequest().getOrder().getCart().getCartItems().get(1).getPictureUri());
+        assertEquals("30.00", request.getOrder().getCart().getCartItems().get(1).getTotalPrice());
+        assertEquals("10.00", request.getOrder().getCart().getCartItems().get(1).getUnitPrice());
+        assertEquals("EUR", request.getOrder().getCart().getCartItems().get(1).getCurrency());
+        assertEquals("3", request.getOrder().getCart().getCartItems().get(1).getQuantity());
+        assertNull(request.getOrder().getCart().getCartItems().get(1).getUniverse());
+        assertNull(request.getOrder().getCart().getCartItems().get(1).getCategory());
+        assertNull(request.getOrder().getCart().getCartItems().get(1).getBrand());
+        assertNull(request.getOrder().getCart().getCartItems().get(1).getModel());
+        assertEquals("ref2", request.getOrder().getCart().getCartItems().get(1).getReference());
+        assertEquals("comment2", request.getOrder().getCart().getCartItems().get(1).getDescription());
+        assertNull(request.getOrder().getCart().getCartItems().get(1).getPictureUri());
 
         // misc data
-        assertNull(request.getRequest().getMiscData());
+        final Map<String, Object> miscData = request.getMiscData();
+        assertNotNull(miscData);
+        assertEquals(2, miscData.size());
+        final Object object1 = miscData.get("cle1");
+        assertTrue(object1 instanceof Map);
+        final Map<String, Object> map1 = (Map<String, Object>) object1;
+        assertEquals(1, map1.size());
+        assertEquals("sousVal", map1.get("sousCle"));
+        final Object object2 = miscData.get("cle2");
+        assertEquals("valeur", object2);
     }
 
-    private void assertCustomer(final FinancingRequest request) {
-        assertEquals("id1", request.getCustomer().getCustomerId());
+    private void assertCustomer(final Customer customer) {
+        assertEquals("id1", customer.getCustomerId());
 
         // identification
-        assertNull(request.getCustomer().getIdentification().getTitle());
-        assertEquals("Durand", request.getCustomer().getIdentification().getSurname());
-        assertEquals("Marie", request.getCustomer().getIdentification().getFirstname());
-        assertNull(request.getCustomer().getIdentification().getBirthName());
-        assertNull(request.getCustomer().getIdentification().getDateOfBirth());
-        assertNull(request.getCustomer().getIdentification().getPlaceOfBirth());
-        assertNull(request.getCustomer().getIdentification().getPlaceOfBirthCode());
-        assertNull(request.getCustomer().getIdentification().getPlaceOfBirthCountry());
+        assertNull(customer.getIdentification().getTitle());
+        assertEquals("Durand", customer.getIdentification().getSurname());
+        assertEquals("Marie", customer.getIdentification().getFirstname());
+        assertNull(customer.getIdentification().getBirthName());
+        assertNull(customer.getIdentification().getDateOfBirth());
+        assertNull(customer.getIdentification().getPlaceOfBirth());
+        assertNull(customer.getIdentification().getPlaceOfBirthCode());
+        assertNull(customer.getIdentification().getPlaceOfBirthCountry());
 
         // account data
-        assertNull(request.getCustomer().getAccountData());
+        assertNull(customer.getAccountData());
 
         // contract data
-        assertEquals("0033601020304", request.getCustomer().getContactData().getMobile());
-        assertEquals("test@mail.com", request.getCustomer().getContactData().getEmail());
+        assertEquals("0033601020304", customer.getContactData().getMobile());
+        assertEquals("test@mail.com", customer.getContactData().getEmail());
 
         // billing address
-        assertEquals("1 billing1" , request.getCustomer().getBillingAddress().getStreetNumberAndNameLine1());
-        assertEquals("billing2" , request.getCustomer().getBillingAddress().getStreetNameLine2());
-        assertEquals("billingCity" , request.getCustomer().getBillingAddress().getTownName());
-        assertEquals("billingZipCode" , request.getCustomer().getBillingAddress().getPostalCode());
-        assertEquals("FR" , request.getCustomer().getBillingAddress().getCountry());
+        assertEquals("1 billing1" , customer.getBillingAddress().getStreetNumberAndNameLine1());
+        assertEquals("billing2" , customer.getBillingAddress().getStreetNameLine2());
+        assertEquals("billingCity" , customer.getBillingAddress().getTownName());
+        assertEquals("billingZipCode" , customer.getBillingAddress().getPostalCode());
+        assertEquals("FR" , customer.getBillingAddress().getCountry());
 
         // shipping address
-        assertEquals("2 delivery1" , request.getCustomer().getShippingAddress().getStreetNumberAndNameLine1());
-        assertNull(request.getCustomer().getShippingAddress().getStreetNameLine2());
-        assertEquals("deliveryCity" , request.getCustomer().getShippingAddress().getTownName());
-        assertEquals("deliveryZipCode" , request.getCustomer().getShippingAddress().getPostalCode());
-        assertEquals("FR" , request.getCustomer().getShippingAddress().getCountry());
+        assertEquals("2 delivery1" , customer.getShippingAddress().getStreetNumberAndNameLine1());
+        assertNull(customer.getShippingAddress().getStreetNameLine2());
+        assertEquals("deliveryCity" , customer.getShippingAddress().getTownName());
+        assertEquals("deliveryZipCode" , customer.getShippingAddress().getPostalCode());
+        assertEquals("FR" , customer.getShippingAddress().getCountry());
     }
 }
